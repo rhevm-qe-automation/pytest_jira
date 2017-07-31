404de93d1427b67e5a0936241bc2f6b8cdab522f
Or Carmi <or.carmi82@gmail.com>
2017-07-31 18:03:40 +0300

    Flipped fixture return value, now return True if issue is open, False if resolved (as it is documented) (#83)

--- pytest_jira.py
+++ pytest_jira.py
@@ -458,6 +458,6 @@ def jira_issue(request):
     def wrapper_jira_issue(issue_id):
         jira_plugin = getattr(request.config, '_jira', None)
         if jira_plugin and jira_plugin.conn.is_connected():
-            return jira_plugin.is_issue_resolved(issue_id)
+            return not jira_plugin.is_issue_resolved(issue_id)
 
     return wrapper_jira_issue
